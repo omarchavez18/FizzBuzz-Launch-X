cc7bb8e04c431cf5dc3c70612bc107779adf302e
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-28 22:24:47 -0500

    se comento codigo

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+//const fs = require("fs");
 
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata); // lista de explorers del archivo json
+// const rawdata = fs.readFileSync("explorers.json");
+// const explorers = JSON.parse(rawdata); // lista de explorers del archivo json
 
 //console.log("Hey I'm here!!");
 //console.log(explorers);
