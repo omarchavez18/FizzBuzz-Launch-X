e01aa5863b0dd9219fd2aad18e979147af573b54
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-30 19:57:40 -0500

    validaciones

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorersService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
-const explorers = Reader.readJjsonFile("explorers.json");
+const explorers = Reader.readJsonFile("explorers.json");
 //console.log(explorers);
 
 // Aplicación del ExplorerService sobre la lista de explorers
 console.log(ExplorerService.filterByMission(explorers, "node"));
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
 
-//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+//
+console.log(FizzbuzzService.applyValidationInExplorer(explorers[4]));
