56e0a31adc33068fc7e699ab031cf20978ad13ae
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-04 22:08:21 -0500

    se arreglaron detalles con lintern

--- test/Reader.test.js
+++ test/Reader.test.js
@@ -1,5 +1,5 @@
 const Reader = require("../lib/utils/Reader");
-const explorers = require("./../explorers.json");
+const explorers = require("../explorers.json");
 describe("testing de Reader.js", () => {
     test("read json file", () => {
         const readerPrueba = Reader.readJsonFile("explorers.json");
