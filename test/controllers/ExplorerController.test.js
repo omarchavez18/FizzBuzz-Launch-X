9fdfb82565166fedfbfbcffd4c3102751e1b43ab
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-08 21:53:59 -0500

    se agrego el testing de explorerController de la  parte de obtener los nombres de usuarios

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,36 +2,40 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 const explorers = require("./../../explorers.json");
 
 describe("pruebas de Explorer Controller", () => {
-    test("prueba 1) obtener el explorer por la mission", () => {
-        const personas = explorers.filter(
-            (explorer) => explorer.mission === "node"
-        );
-        const datosARevisar = ExplorerController.getExplorersByMission("node");
-        datosARevisar.forEach((persona, i) => {
-            expect(persona.mission).toBe(personas[i].mission);
-        });
+  test("prueba 1) obtener el explorer por la mission", () => {
+    const personas = explorers.filter(
+      (explorer) => explorer.mission === "node"
+    );
+    const datosARevisar = ExplorerController.getExplorersByMission("node");
+    datosARevisar.forEach((persona, i) => {
+      expect(persona.mission).toBe(personas[i].mission);
     });
+  });
 
-    // test("prueba 2) obtener los nombres de los usuarios", () => {
-    //     const grupoExplorers = explorers.filter(
-    //         (explorer) => explorer.mission === "node"
-    //     );
+  test("prueba 2) obtener los nombres de los usuarios por la mision enviada", () => {
+    const gente = explorers
+      .filter((explorer) => explorer.mission === "node")
+      .map((explorer) => explorer.githubUsername);
 
-    //     const infoARevisar =
-    //   ExplorerController.getExplorersUsernamesByMission("node");
-    //     infoARevisar.forEach((personaDelGrupo) => {
-    //         expect(personaDelGrupo).toBe(grupoExplorers.githubUserName);
-    //     });
-    // });
+    const personas = ExplorerController.getExplorersUsernamesByMission("node");
+    expect(personas).toEqual(gente);
+  });
 
-    // test("prueba 3) obtener la cantidad de usuarios", () => {
-    //     const personas = explorers.filter(
-    //         (explorer) => explorer.mission === "node"
-    //     );
-    //     const info = ExplorerController.getExplorersAmountByMission("node");
+  //     const infoARevisar =
+  //   ExplorerController.getExplorersUsernamesByMission("node");
+  //     infoARevisar.forEach((personaDelGrupo) => {
+  //         expect(personaDelGrupo).toBe(grupoExplorers.githubUserName);
+  //     });
+  // });
 
-    //     expect(info.length).toBe(personas.length);
-    // });
+  // test("prueba 3) obtener la cantidad de usuarios", () => {
+  //     const personas = explorers.filter(
+  //         (explorer) => explorer.mission === "node"
+  //     );
+  //     const info = ExplorerController.getExplorersAmountByMission("node");
 
-    test("prueba 4) obtener 'fizz', 'buzz', 'fizzbuzz', o el mismo score con el number proporcionado", () => {});
+  //     expect(info.length).toBe(personas.length);
+  // });
+
+  //  test("prueba 4) obtener 'fizz', 'buzz', 'fizzbuzz', o el mismo score con el number proporcionado", () => {});
 });
